e274965fef65917809be4cde1e8c1dbeadf76707
Terry Babyuk <tbabyuk@gmail.com>
2023-01-03 00:40:17 -0500

    added date feature in date-feature branch

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
 console.log("hello world!")
+
+const date = document.querySelector("#date");
+
+const lang = navigator.language;
+const now = new Date();
+
+const today = now.toLocaleString(lang, {dateStyle: "full"});
+
+date.innerText = today;
